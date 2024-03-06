d714a73cc55951b954415f341fa4b0d70fc096a6
Lorbasta <107131564+pastadozembow@users.noreply.github.com>
2024-03-06 19:45:01 +0100

    Update A2.py

--- A2.py
+++ A2.py
@@ -4,7 +4,7 @@ tb = 2*b
 tc = 3*c
 n = tb + tc
 '''
-i = 10 kod by jan krasinski
+i = 10 kod by jan krasinski, pola brejt, wojciech gruntkowski i jeremi ogrodowczyk
 '''
 sum = c + b + n
 print(sum)
