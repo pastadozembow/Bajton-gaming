6daf64a4a386ef6504042b15e6fd459723059a6c
Lorbasta <107131564+pastadozembow@users.noreply.github.com>
2024-03-06 19:46:09 +0100

    Update A6.py

--- A6.py
+++ A6.py
@@ -2,7 +2,7 @@ import math
 a, b, x = map(float, input().split())
 if(x>=a):
     '''
-i = 10 kod by jan krasinski
+i = 10 kod by jan krasinski, pola brejt, wojciech gruntkowski i jeremi ogrodowczyk
 '''
     if(x<=b):
         print("Tak")
