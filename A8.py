892d67a5324ca957de6926ec745310a86b948396
Lorbasta <107131564+pastadozembow@users.noreply.github.com>
2024-03-06 19:46:32 +0100

    Update A8.py

--- A8.py
+++ A8.py
@@ -1,7 +1,7 @@
 p = int(input())
 suma = 0
 '''
-i = 10 kod by jan krasinski
+i = 10 kod by jan krasinski, pola brejt, wojciech gruntkowski i jeremi ogrodowczyk
 '''
 for i in range(p):
     a = float(input())
