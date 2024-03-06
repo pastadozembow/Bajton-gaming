f6311c3d10035100e390aa95ece2cbe8627cddee
Lorbasta <107131564+pastadozembow@users.noreply.github.com>
2024-03-06 19:48:21 +0100

    Update B05.py

--- B05.py
+++ B05.py
@@ -2,7 +2,7 @@ text = input()
 res = [int(i) for i in text.split() if i.isdigit()]
 suma = 0
 '''
-i = 10 kod by pola brejt
+i = 10 kod by pola brejt, jan krasinski, wojciech gruntkowski i jeremi ogrodowczyk
 '''
 for o in range(len(res)):
   suma += res[o]
