8c7c95624a1c6dc149ee87b20eca3d917ef6d5f1
Lorbasta <107131564+pastadozembow@users.noreply.github.com>
2024-03-06 19:45:39 +0100

    Update A4.py

--- A4.py
+++ A4.py
@@ -3,6 +3,6 @@ if(a % 2 == 0):
     print("Liczba parzysta")
 else:
     '''
-i = 10 kod by jan krasinski
+i = 10 kod by jan krasinski, pola brejt, wojciech gruntkowski i jeremi ogrodowczyk
 '''
     print("Liczba nieparzysta")
