a331becf70d1f12bb637bb0f9bd7012d606c89eb
Lorbasta <107131564+pastadozembow@users.noreply.github.com>
2024-03-06 19:47:05 +0100

    Update B01.py

--- B01.py
+++ B01.py
@@ -4,7 +4,7 @@ h = float(input())
 bok = 0
 pole = 0
 '''
-i = 10 kod by jan krasinski
+i = 10 kod by jan krasinski, pola brejt, wojciech gruntkowski i jeremi ogrodowczyk
 '''
 bok = 2 *h / math.sqrt(3)
 pole = bok * h / 2
